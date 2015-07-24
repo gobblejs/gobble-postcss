cd64f92c91f2ad415b4e20c39d2c3201b90d0f73
Rich Harris <richard.a.harris@gmail.com>
2015-07-24 12:15:10 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# gobble-postcss
+
+Post-process CSS files with gobble and [postcss](https://github.com/postcss/postcss).
+
+## Installation
+
+First, you need to have gobble installed - see the [gobble readme](https://github.com/gobblejs/gobble) for details. Then,
+
+```bash
+npm i -D gobble-postcss
+```
+
+## Usage
+
+```js
+var gobble = require( 'gobble' );
+module.exports = gobble( 'src/styles' ).transform( 'postcss', {
+  plugins: [
+    require( 'postcss-import' ),
+    require( 'autoprefixer-core', { browsers: [ 'last 2 versions' ] }),
+    require( 'cssnano' )
+  ],
+
+  // if `src` is omitted, all CSS files will be processed. If you're
+  // using `postcss-import`, that's probably not what you want. It
+  // can be a glob pattern
+  src: 'main.css',
+
+  // `dest` can be a string if `src` is supplied and is *not* a glob
+  // pattern. Otherwise it must be a function. If omitted, dest name
+  // will be same is src name
+  dest: function ( src ) {
+    // e.g. main.css -> main.min.css
+	return src.replace( '.css', '.min.css' );
+  }
+});
+```
+
+
+## License
+
+MIT. Copyright 2015 Rich Harris
---
+++ index.js
@@ -0,0 +1,45 @@
+var path = require( 'path' );
+var glob = require( 'glob' );
+var sander = require( 'sander' );
+var _postcss = require( 'postcss' );
+
+var Promise = sander.Promise;
+
+function identity ( input ) {
+	return input;
+}
+
+module.exports = function postcss ( inputdir, outputdir, options ) {
+	var processor = _postcss( options.plugins );
+
+	var files = glob.sync( options.src || '**/*.css', {
+		cwd: inputdir
+	});
+
+	if ( typeof options.dest === 'string' && ( typeof options.src !== 'string' || !~options.src.indexOf( '*' ) ) ) {
+		throw new Error( 'options.dest can only be a string if options.src is a string, and not a glob pattern' );
+	}
+
+	var getDest = typeof options.dest === 'string' ?
+		function () { return options.dest; } :
+		typeof options.dest === 'function' ?
+			options.dest :
+			identity;
+
+	var promises = files.map( function ( src ) {
+		var dest = getDest( src );
+
+		return sander.readFile( inputdir, src )
+			.then( function ( css ) {
+				return processor.process( css, {
+					from: path.join( inputdir, src ),
+					to: path.join( outputdir, dest )
+				}).then( function ( result ) {
+					// TODO sourcemaps
+					return sander.writeFile( outputdir, dest, result.css );
+				});
+			});
+	});
+
+	return Promise.all( promises );
+};
